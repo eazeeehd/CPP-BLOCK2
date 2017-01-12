cb6b88c6870b241d2da1a92bd840121fbc812540
Peter Kemley <eazeeehd@hotmail.com>
2017-01-12 10:15:18 +0000

    Update class.cpp

--- OOP/class.cpp
+++ OOP/class.cpp
@@ -8,5 +8,20 @@ using namespace std;
 //define class for files
 class File
 {
+  //creation of file class has private data members of name and size
+ private:
+  string fname;
+  int fsize;
+  
+  // default constructor - used when object created
+  //set and get methods for each of the data items
+  // all are public
+  public:
+  // constructor same name as class
+  File()
+  {// set data items to default value
+     fsize = 0;
+     fname.assign("");
+  }//end of constructor
 
-};
+};//end of class definition
